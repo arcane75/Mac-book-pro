ebd09acab7d31dbfcfb35e78f5d69aa0fc5cee0f
unknown <mdrakibulhoque83@gmail.com>
2021-08-21 22:19:38 +0600

    done

--- js/mac-book.js
+++ js/mac-book.js
@@ -1,4 +1,3 @@
-// Get id and store it
 const totalPrice = document.getElementById('totalPrice');
 const grandAmount = document.getElementById('grand-total');
 const extraMemoryCost = document.getElementById('extra-memory');
@@ -6,38 +5,61 @@ const extraStorageCost = document.getElementById('extra-storage');
 const extraDeliveryCost = document.getElementById('delivery-charge');
 const promoInput = document.getElementById('promoCode-input');
 
-//------------------------Memory Selection Section--------------------------
+
+//Update Memory total Cost
+function updateMemoryCost(product, price) {
+    const memoryPrice = price;
+    const extraMemoryCost = document.getElementById('extra-memory');
+    extraMemoryCost.innerText = memoryPrice;
+
+}
+//Update Storage Total Cost
+function updateStorageCost(product, price) {
+    const storagePrice = price;
+    const extraStorageCost = document.getElementById('extra-storage');
+    extraStorageCost.innerText = storagePrice;
+}
+// Update Delivery Total Cost
+function updateDeliveryCost(product, price) {
+    const deliveryCost = price;
+    const extraDeliveryCost = document.getElementById('delivery-charge');
+    extraDeliveryCost.innerText = deliveryCost;
+}
+
+
+
+//Memory Selection
 document.getElementById("eightGB-memory-btn").addEventListener('click', function () {
-    extraMemoryCost.innerText = '0';
+    updateMemoryCost('memory', 0);
     calculateTotal();
 })
 document.getElementById("sixteenGB-memory-btn").addEventListener('click', function () {
-    extraMemoryCost.innerText = '180';
+    updateMemoryCost('memory', 180);
     calculateTotal();
 })
 
-//------------------------Storage Selection Section--------------------------
+
+//Storage Selection
 document.getElementById("ssd256-btn").addEventListener('click', function () {
-    extraStorageCost.innerText = '0';
+    updateStorageCost('storage', 0);
     calculateTotal();
 })
 document.getElementById("ssd512-btn").addEventListener('click', function () {
-    extraStorageCost.innerText = '100';
+    updateStorageCost('storage', 100);
     calculateTotal();
 })
 document.getElementById("ssd1TB-btn").addEventListener('click', function () {
-    extraStorageCost.innerText = '180';
+    updateStorageCost('storage', 180);
     calculateTotal();
 })
 
-// --------------------------Shipping Method Section--------------------------
+// Shipping Method
 document.getElementById("free-delivery").addEventListener('click', function () {
-    //updateDeliveryCost('delivery', 0);
-    extraDeliveryCost.innerText = '0';
+    updateDeliveryCost('delivery', 0);
     calculateTotal();
 })
 document.getElementById("paid-delivery").addEventListener('click', function () {
-    extraDeliveryCost.innerText = '20';
+    updateDeliveryCost('delivery', 20);
     calculateTotal();
 })
 
@@ -54,6 +76,7 @@ function calculateTotal() {
    // Return total price
     return totalPrice.innerText;
 }
+
 // --------------------------Promo code Section--------------------------
 document.getElementById("promoCode-applyBtn").addEventListener("click", function () {
     promoCode();
@@ -77,52 +100,3 @@ function promoCode() {
     promoInput.value = '';
 }
 
-
-
-
-
-
-
-
-
-
-
-/* //DISABLING 8GB Memory BUTTON
-function eightGBButtonDisable() {
-    document.getElementById('eightGB-memory-btn').disabled = true;
-    document.getElementById('sixteenGB-memory-btn').disabled = false;
-}
-//DISABLING 16GB Memory BUTTON
-function sixteenGBButtonDisable() {
-    document.getElementById('eightGB-memory-btn').disabled = false;
-    document.getElementById('sixteenGB-memory-btn').disabled = true;
-}
-//DISABLING 256GB Storage BUTTON
-function storage256ButtonDisable() {
-    document.getElementById('ssd256-btn').disabled = true;
-    document.getElementById('ssd512-btn').disabled = false;
-    document.getElementById('ssd1TB-btn').disabled = false;
-}
-//DISABLING 512GB Storage BUTTON
-function storage512ButtonDisable() {
-    document.getElementById('ssd256-btn').disabled = false;
-    document.getElementById('ssd512-btn').disabled = true;
-    document.getElementById('ssd1TB-btn').disabled = false;
-}
-//DISABLING 1TB Storage BUTTON
-function storage1TBButtonDisable() {
-    document.getElementById('ssd256-btn').disabled = false;
-    document.getElementById('ssd512-btn').disabled = false;
-    document.getElementById('ssd1TB-btn').disabled = true;
-}
-//Free delivery Button Disabled
-function freeDelButtonDisable() {
-    document.getElementById('free-delivery').disabled = true;
-    document.getElementById('paid-delivery').disabled = false;
-}
-//Paid delivery Button Disabled
-function paidDelButtonDisable() {
-    document.getElementById('free-delivery').disabled = false;
-    document.getElementById('paid-delivery').disabled = true;
-}
- */
